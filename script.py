140adb691e2f3f386247fbbbd7972c6bba807af6
Jacob Parra <jacobisraelparra@gmail.com>
2021-03-06 20:35:01 -0700

    Adding more

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 import bpy
 from bpy import context
 
-def generate_render(file_path):
+# General Functions
+def render(file_path):
     bpy.ops.object.camera_add(enter_editmode=False, align="VIEW", location=(0, -5, 4.00), rotation=(45, 0, 0))
     context.scene.camera = context.object
     bpy.ops.object.light_add(type='POINT', radius=1, align="WORLD", location=(0, 0, 1))
@@ -15,14 +16,30 @@ def clear_all_objects():
         obj.select_set(True)
     bpy.ops.object.delete()
 
-def create_primitive_monkey():
-    bpy.ops.mesh.primitive_monkey_add(location=(0.00,0.00,1.00))
-
 def save_blender_file(file_path):
     bpy.ops.wm.save_as_mainfile(filepath=file_path)
 
+# Modeling
+def model():
+    create_primitive_sphere()
+    add_sphere_modifiers()
+
+
+def add_sphere_modifiers():
+    bpy.ops.object.modifier_add(type='SUBSURF')
+    bpy.ops.object.shade_smooth()
+    
+
+def create_primitive_sphere():
+    bpy.ops.mesh.primitive_uv_sphere_add(location=(0.00,0.00,1.00))
+
 if __name__ == '__main__':
     clear_all_objects()
-    create_primitive_monkey()
-    generate_render("model.png")
+    model()
+    render("model.png")
+    # uv_map()
+    # texture()
+    # rig()
+    # animate()
+    # light()
     save_blender_file("script.blend")
